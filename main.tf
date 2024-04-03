91bbe186ad3f9e74b396281049fd0f28aa852ceb
Aarti Joshi <aarti.joshi@nttdata.com>
2024-04-03 09:25:32 -0400

    Examples and code

--- main.tf
+++ main.tf
@@ -14,22 +14,23 @@ resource "azurerm_network_interface" "network_interface" {
   name                          = var.name
   location                      = var.location
   resource_group_name           = var.resource_group_name
-  auxiliary_mode                = var.auxillary_mode
-  auxiliary_sku                 = var.auxiliary_sku
   dns_servers                   = var.dns_servers
   edge_zone                     = var.edge_zone
   enable_ip_forwarding          = var.enable_ip_forwarding
   enable_accelerated_networking = var.enable_accelerated_networking
   internal_dns_name_label       = var.internal_dns_name_label
-  tags                          = var.tags
-  ip_configuration {
-    name                                               = var.ip_configuration.name
-    gateway_load_balancer_frontend_ip_configuration_id = var.ip_configuration.gateway_load_balancer_frontend_ip_configuration_id
-    subnet_id                                          = var.ip_configuration.subnet_id
-    private_ip_address_version                         = var.ip_configuration.private_ip_address_version
-    private_ip_address_allocation                      = var.ip_configuration.private_ip_address_allocation
-    public_ip_address_id                               = var.ip_configuration.public_ip_address_id
-    primary                                            = var.ip_configuration.primary
-    private_ip_address                                 = var.ip_configuration.private_ip_address
+  tags                          = local.tags
+  dynamic "ip_configuration" {
+    for_each = var.ip_configuration
+    content {
+      name                                               = ip_configuration.value.name
+      gateway_load_balancer_frontend_ip_configuration_id = ip_configuration.value.gateway_load_balancer_frontend_ip_configuration_id
+      subnet_id                                          = ip_configuration.value.subnet_id
+      private_ip_address_version                         = ip_configuration.value.private_ip_address_version
+      private_ip_address_allocation                      = ip_configuration.value.private_ip_address_allocation
+      public_ip_address_id                               = ip_configuration.value.public_ip_address_id
+      primary                                            = ip_configuration.value.primary
+      private_ip_address                                 = ip_configuration.value.private_ip_address
+    }
   }
 }
